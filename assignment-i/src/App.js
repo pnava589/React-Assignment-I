e89d4f161bf2188b196a37ddbd3a9c651982f6be
Juan Jimenez <juanjimenez@Juans-MacBook-Pro.local>
2020-02-09 14:13:54 -0700

    added home component

--- assignment-i/src/App.js
+++ assignment-i/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './About'
+import Home from './components/Home';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+     <Home/>
     </div>
   );
 }
---
+++ assignment-i/src/components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+class Home extends React.Component{
+    render(){
+        return(
+            <div>Juan</div>
+        );
+    };
+}
+export default Home;
