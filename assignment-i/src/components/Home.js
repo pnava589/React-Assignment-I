3f2432b501bd450427e82287018b41fcfd333327
Juan Jimenez <juanjimenez@Juans-MacBook-Pro.local>
2020-02-09 16:43:41 -0700

    Restructure home to have card componenet

--- assignment-i/src/components/Home.js
+++ assignment-i/src/components/Home.js
@@ -1,33 +1,16 @@
 import React from "react";
+import HomeCard from "./HomeCard";
 class Home extends React.Component{
     render(){
         const imgSrc = "./felix-mooneeram-evlkOfkQ5rE-unsplash.jpg";
         return(
             <div className="container-fluid"  style = {{ backgroundImage: `url(${imgSrc})`,height: '800px',
             backgroundSize: 'cover', backgroundPosition: 'center center', backgroundRepeat: 'no-repeat',}}>
-               <div className="h-100 row align-items-center">
-                <div className="col-md-6 offset-md-3">
-                      <div className="card text-center">
-                        <h5 className="card-header">
-                          Movie Browser
-                        </h5>
-                        <div className="card-body">
-                          <div className="card-text">
-                            <div className="row">
-                            <label className="col-md-2"><b>Title:</b></label><input className="col-md-8" type="text"></input>
-                            </div>
-                            <br></br>
-                            <div className="row text-white">
-                              <a className="col-md-4 offset-md-2 btn btn-primary">Show Matching Movies</a>
-                              <a className="col-md-3 offset-md-1 btn btn-primary">Show All Movies</a>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                  </div>
+                <div className="h-100 row align-items-center">
+                  <HomeCard/>
                   <div className="col-md-6 offset-5 text-white">
                     Photo Credit: <a href="https://unsplash.com/@felixmooneeram">Felix Mooneeram</a>
-                  </div>
+                </div>
                 </div>
             </div>
         );
---
+++ assignment-i/src/components/HomeCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+const HomeCard = () => 
+                <div className="col-md-6 offset-md-3">
+                      <div className="card text-center">
+                        <h5 className="card-header">
+                          Movie Browser
+                        </h5>
+                        <div className="card-body">
+                          <div className="card-text">
+                            <div className="row">
+                            <label className="col-md-2"><b>Title:</b></label><input className="col-md-8" type="text"></input>
+                            </div>
+                            <br></br>
+                            <div className="row text-white">
+                              <a className="col-md-4 offset-md-2 btn btn-primary">Show Matching Movies</a>
+                              <a className="col-md-3 offset-md-1 btn btn-primary">Show All Movies</a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                  </div>;
+                  
+export default HomeCard;
