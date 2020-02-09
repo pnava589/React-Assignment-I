04b5ebf72a7e566f0fd1f973a6685aa6129f9ded
Pedro <35156354+pnava589@users.noreply.github.com>
2020-02-09 14:57:56 -0700

    pushing pedro's changes

--- assignment-i/src/components/Home.js
+++ assignment-i/src/components/Home.js
@@ -7,7 +7,7 @@ class Home extends React.Component{
               <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                  Edit <code>src/App.js</code> and save to reload.
+                  Edit <code>new changes made by pedro</code> and save to reload.
                 </p>
                 <a
                   className="App-link"
