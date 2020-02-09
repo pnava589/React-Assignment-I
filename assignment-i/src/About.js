651c2f464acaeb9f75634dd3d77e39f9b4b659ba
Pedro <35156354+pnava589@users.noreply.github.com>
2020-02-09 13:53:52 -0700

    added about page

---
+++ assignment-i/src/About.js
@@ -0,0 +1,33 @@
+import React from "react";
+//import { Link } from 'react-router-dom';
+import photo from './logo.svg';
+
+
+class About extends React.Component {
+ render() {
+    
+ return (
+    <div className = 'banner'
+    style = {{ backgroundImage: `url(${photo})`,
+    height: '800px',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center center',
+    backgroundRepeat: 'no-repeat',
+    }}>
+        <div >
+            <h1>About</h1>
+            <h3>More information coming up <br/>Stay tuned</h3>
+            <p>
+                
+                    <button>Browse</button>
+                
+                
+                    <button>About</button>
+                
+            </p>
+        </div>
+    </div>
+ );
+ }
+}
+export default About
