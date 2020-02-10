0a295068bf417ae5f97915bbb81e0b8081775f48
Pedro <35156354+pnava589@users.noreply.github.com>
2020-02-09 18:55:25 -0700

    added extra content of About page

--- assignment-i/src/components/About.js
+++ assignment-i/src/components/About.js
@@ -10,7 +10,7 @@ class About extends React.Component {
         <div>
         <div className="jumbotron">
             <div className="container">
-                <h1 className="display-3"> About </h1>
+                <h1 className="display-4"> About </h1>
                 
                 
                 <p>This page provides information regarding this porject and the referenced sources </p>
@@ -32,6 +32,16 @@ class About extends React.Component {
                 <ul>
                     <li>Bootstrap Documentation</li>
                     
+                </ul>
+                <h1 className="blog-post">Github Link</h1>
+                <a href="https://github.com/pnava589/React-Assignment-I.git">Repo</a>
+
+                <h1 className="blog-post">Technology Used</h1>
+                <ul>
+                    <li>Bootstrap for styiling</li>
+                    <li>React</li>
+                    <li>PHP API's</li>
+
                 </ul>
                 
             </div>
