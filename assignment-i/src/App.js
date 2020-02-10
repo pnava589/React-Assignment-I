cbf312f571fa7166c9c221e24b34f00c433497c4
Juan Jimenez <juanjimenez@Juans-MacBook-Pro.local>
2020-02-09 17:31:05 -0700

    browser router add and header

--- assignment-i/src/App.js
+++ assignment-i/src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 import './App.css';
 import './components/About'
 import Home from './components/Home';
-
+import Default from './components/Default';
+import { Route } from 'react-router-dom';
+import About from './components/About';
 
 class App extends React.Component{
   constructor(props){
@@ -13,7 +15,10 @@ class App extends React.Component{
   render(){
     return(
       <main>
-        <Home/>
+        <Route path='/' exact component={Home} />
+        <Route path='/home' exact component={Home} />
+        <Route path='/default' exact component={Default} />
+        <Route path='/about' exact component={About}/>
       </main>
     );
   };
--- assignment-i/src/components/DefaultHeader.js
+++ assignment-i/src/components/DefaultHeader.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const DefaultHeader = () =>
-<div>
-
-</div>;
+        <nav className="navbar navbar-dark bg-dark">
+            <Link to='/home'>
+                <a className="navbar-brand">Logo</a>
+            </Link>
+            <Link to='/about'>
+            <a className="btn btn-primary my-2 text-white">About</a>
+            </Link>
+        </nav>;
 
 export default DefaultHeader;
