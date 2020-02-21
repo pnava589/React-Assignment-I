aeaa5940a76d50fb3bd4a2a9c4bc2dfa6ae17322
juanmoretime <jjime368@mtroyal.ca>
2020-02-20 21:08:00 -0700

    style close box

--- assignment-i/src/components/FavoriteItem.js
+++ assignment-i/src/components/FavoriteItem.js
@@ -9,10 +9,19 @@ class FavoriteItem extends React.Component{
         this.props.remove(this.state.id);
     }
     render(){
+        const closeStyle = {
+            position: "absolute",
+            top: "0px",
+            right: "15px",
+            paddingTop: "0",
+            marginTop: "0",
+            marginRight: "2px",
+            color: "red"
+        }
         const imgUrl = "https://image.tmdb.org/t/p/w92"+this.state.poster;
         return(
             <div className="col-md-1 align-self-center ">
-                <button type="button" className="close" aria-label="Close" onClick={this.remove}>
+                <button type="button" style={closeStyle} className="close" aria-label="Close" onClick={this.remove}>
                     <span aria-hidden="true">&times;</span>
                 </button>
                 <Link to={`/details/${this.state.id}`} id={this.state.id}>
